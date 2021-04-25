7da2dfe614af6bdf7355e79e9466a24a841248f7
unknown <mojnushah2018@gmail.com>
2021-04-25 17:51:29 +0600

    dynamic navbar

--- src/Pages/SignIn/SignIn.js
+++ src/Pages/SignIn/SignIn.js
@@ -5,21 +5,7 @@ import React from 'react';
 const SignIn = () => {
     return (
         <div>
-            {/* <Container>
-                {/* <FormWrap>
-                    <Icon to='/'>Virtual</Icon>
-                    <FormContent>
-                        <Form action='#'>
-                            <FromH1>Sign In Your Account</FromH1>
-                            <FormLabel>Email</FormLabel>
-                            <FormLabel htmlFor='for'></FormLabel>
-                            <FormInput type='email' required/>                    
-                            <FormLabel htmlFor='for'>Password</FormLabel>
-                            <FormInput type='password' required/>
-                        </Form>
-                    </FormContent>
-                </FormWrap>
-            </Container> */} */}
+           
         </div>
     );
 };
